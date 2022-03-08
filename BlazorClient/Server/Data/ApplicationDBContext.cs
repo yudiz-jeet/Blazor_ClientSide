f5c6a8eafb530f1cfca3a080d6243fccee0e20d4
Yudiz-jeet <jeet.limbani@yudiz.com>
2022-03-08 10:40:02 +0530

    Add

---
+++ BlazorClient/Server/Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using BlazorClient.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorClient.Server.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Developer> Developers { get; set; }
+    }
+}
